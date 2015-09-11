f38ca650cb82b908dd0760eb2b33d85dc8165029
Y <y.naturals.tech@gmail.com>
2015-09-11 13:15:58 +0800

    init

---
+++ Contact_Us_files/Contact_Us.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120429
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_2:new IWShadow({blurRadius:2,offset:new IWPoint(0.5513,1.9225),color:'#6f6f6f',opacity:1.000000}),shadow_1:new IWShadow({blurRadius:6,offset:new IWPoint(-0.1396,3.9976),color:'#000000',opacity:1.000000}),stroke_1:new IWPhotoFrame([IWCreateImage('Contact_Us_files/FormalShadow_01.png'),IWCreateImage('Contact_Us_files/FormalShadow_02.png'),IWCreateImage('Contact_Us_files/FormalShadow_03.png'),IWCreateImage('Contact_Us_files/FormalShadow_06.png'),IWCreateImage('Contact_Us_files/FormalShadow_12.png'),IWCreateImage('Contact_Us_files/FormalShadow_11.png'),IWCreateImage('Contact_Us_files/FormalShadow_10.png'),IWCreateImage('Contact_Us_files/FormalShadow_04.png')],null,2,0.858388,1.000000,5.000000,1.000000,4.000000,17.000000,17.000000,17.000000,36.000000,4.000000,837.000000,4.000000,837.000000,null,null,null,0.100000),shadow_3:new IWShadow({blurRadius:12,offset:new IWPoint(-0.2094,5.9963),color:'#000000',opacity:0.750000}),stroke_0:new IWEmptyStroke(),shadow_0:new IWShadow({blurRadius:12,offset:new IWPoint(-0.2094,5.9963),color:'#000000',opacity:1.000000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Contact_Us_files/Contact_UsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');IMpreload('Contact_Us_files','shapeimage_15','0');applyEffects()}
---
+++ Customers_files/Customers.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20120429
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_1:new IWShadow({blurRadius:6,offset:new IWPoint(-0.1396,3.9976),color:'#000000',opacity:1.000000}),stroke_0:new IWEmptyStroke(),stroke_1:new IWPhotoFrame([IWCreateImage('Customers_files/FormalShadow_01.png'),IWCreateImage('Customers_files/FormalShadow_02.png'),IWCreateImage('Customers_files/FormalShadow_03.png'),IWCreateImage('Customers_files/FormalShadow_06.png'),IWCreateImage('Customers_files/FormalShadow_12.png'),IWCreateImage('Customers_files/FormalShadow_11.png'),IWCreateImage('Customers_files/FormalShadow_10.png'),IWCreateImage('Customers_files/FormalShadow_04.png')],null,2,0.858388,1.000000,5.000000,1.000000,4.000000,17.000000,17.000000,17.000000,36.000000,4.000000,837.000000,4.000000,837.000000,null,null,null,0.100000),shadow_0:new IWShadow({blurRadius:12,offset:new IWPoint(-0.2094,5.9963),color:'#000000',opacity:1.000000}),shadow_2:new IWShadow({blurRadius:2,offset:new IWPoint(0.5513,1.9225),color:'#6f6f6f',opacity:1.000000}),shadow_3:new IWShadow({blurRadius:12,offset:new IWPoint(-0.2094,5.9963),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Customers_files/CustomersMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');IMpreload('Customers_files','shapeimage_14','0');applyEffects()}
